d08148da4c018f0159d3c566806f032ed08c4dbb
Fernando Barraza <fernandobarraza@192.168.1.13>
2021-08-30 14:32:45 -0600

    Added the listen options for the events and notifications inside the component

--- resources/assets/js/vue-components/hola.vue
+++ resources/assets/js/vue-components/hola.vue
@@ -6,6 +6,20 @@
 
 <script>
 export default {
-    name: "hola"
+    name: "hola",
+
+    created() {
+
+        // Echo.channel('events')
+        //     .listen('RealTimeMessage', (e) => console.log('RealTimeMessage: ' + e.message));
+
+        // Echo.private('events')
+        //     .listen('RealTimeMessage', (e) => console.log('Private RealTimeMessage: ' + e.message));
+
+        Echo.private('App.Models.User.1')
+        .notification((notification) => {
+            console.log(notification.message)
+        })
+    }
 }
 </script>
